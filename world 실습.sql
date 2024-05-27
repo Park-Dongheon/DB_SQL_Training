d96b9dd05d3bba9dd724285ac03f20f568329d9c
Park-Dongheon <cdsded@naver.com>
2024-05-27 09:11:54 +0900

    first commit

---
+++ world 실습.sql
@@ -0,0 +1,54 @@
+-- SELECT count(*)
+-- FROM city
+-- GROUP BY CountryCode;
+
+-- SELECT Name, Population
+-- FROM city
+-- WHERE CountryCode='kor' and Population>1000000 and Population < 5000000
+-- ORDER BY Name Desc;
+
+-- SELECT count(*)
+-- from city
+-- WHERE CountryCode='kor' and Population>1000000 and Population < 5000000;
+
+-- SELECT CountryCode, Population
+-- FROM city
+-- ORDER BY CountryCode ASC, Population DESC;
+
+-- SELECT Name
+-- from city
+-- WHERE CountryCode IN ('KOR', 'CHN', 'JPN');
+
+-- SELECT Name, Population
+-- FROM city
+-- WHERE CountryCode='kor' and Population>1000000
+-- ORDER BY LIMIT;
+
+
+
+-- SELECT Code
+-- FROM country
+-- WHERE Name LIKE "%KOREA%" ;
+
+-- 방법 1*******실패
+SELECT cl.Language, cl.IsOfficial, cl.Percentage
+FROM countrylanguage cl
+WHERE EXISTS(
+	SELECT
+);
+
+-- 방법 2
+-- SELECT cl.Language, cl.IsOfficial, cl.Percentage
+-- FROM countrylanguage cl
+-- JOIN country c ON cl.CountryCode = c.Code
+-- WHERE c.Name = "South Korea";
+
+-- 방법 3
+-- SELECT b.Language, b.IsOfficial, b.Percentage
+-- FROM country a, countrylanguage b
+-- WHERE a.Name = "SOUTH KOREA" AND a.Code = b.CountryCode;
+
+-- SELECT Name, count(*)
+-- FROM city
+-- GROUP BY Name
+-- HAVING count(*) >= 10;
